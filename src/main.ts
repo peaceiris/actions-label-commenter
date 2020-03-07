5bce3e683ab81ebc68e2ea88ff39e0a86ac0d2e6
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-03-08 01:50:28 +0900

    feat: print payload [skip ci]

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import {Inputs} from './interfaces';
 import {getInputs} from './get-inputs';
 
@@ -6,6 +7,10 @@ export async function run(): Promise<void> {
   try {
     const inps: Inputs = getInputs();
     core.info(`[INFO] config_file: ${inps.ConfigFilePath}`);
+
+    const context = github.context;
+    core.info(`[INFO] context ${context}`);
+    core.info(`[INFO] payload ${context.payload}`);
   } catch (error) {
     throw new Error(error.message);
   }
