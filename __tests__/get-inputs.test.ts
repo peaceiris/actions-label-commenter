de6b95c9f950a620040eebed333781ba1e62064b
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-06-22 05:03:47 +0900

    chore: fix format

--- __tests__/get-inputs.test.ts
+++ __tests__/get-inputs.test.ts
@@ -5,9 +5,7 @@ import yaml from 'js-yaml';
 
 beforeEach(() => {
   jest.resetModules();
-  const doc = yaml.safeLoad(
-    fs.readFileSync(__dirname + '/../action.yml', 'utf8')
-  );
+  const doc = yaml.safeLoad(fs.readFileSync(__dirname + '/../action.yml', 'utf8'));
   Object.keys(doc.inputs).forEach(name => {
     const envVar = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`;
     process.env[envVar] = doc.inputs[name]['default'];
@@ -15,9 +13,7 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-  const doc = yaml.safeLoad(
-    fs.readFileSync(__dirname + '/../action.yml', 'utf8')
-  );
+  const doc = yaml.safeLoad(fs.readFileSync(__dirname + '/../action.yml', 'utf8'));
   Object.keys(doc.inputs).forEach(name => {
     const envVar = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`;
     delete process.env[envVar];
@@ -36,14 +32,11 @@ describe('getInputs()', () => {
 
   test('get spec inputs', () => {
     process.env['INPUT_GITHUB_TOKEN'] = 'secret_token';
-    process.env['INPUT_CONFIG_FILE'] =
-      '.github/config/label-commenter-config.yml';
+    process.env['INPUT_CONFIG_FILE'] = '.github/config/label-commenter-config.yml';
 
     const inps: Inputs = getInputs();
 
     expect(inps.GithubToken).toMatch('secret_token');
-    expect(inps.ConfigFilePath).toMatch(
-      '.github/config/label-commenter-config.yml'
-    );
+    expect(inps.ConfigFilePath).toMatch('.github/config/label-commenter-config.yml');
   });
 });
