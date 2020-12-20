4ba413a85101cb255e3fb9293dbf462e8253dc5a
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-12-20 20:59:20 +0900

    fix:

--- src/main.ts
+++ src/main.ts
@@ -238,7 +238,7 @@ export async function run(): Promise<void> {
         }
       );
       groupConsoleLog('issuesCreateCommentResponse', issuesCreateCommentResponse, core.isDebug());
-      core.info(`${issuesCreateCommentResponse.data.html_url}`);
+      core.info(`[INFO] comment URL: ${issuesCreateCommentResponse.data.html_url}`);
     }
 
     // Close or Open an issue
