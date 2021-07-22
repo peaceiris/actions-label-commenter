f6821372b55851ad219b9f6d76713e9ceebd3f91
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-07-22 12:02:18 +0900

    build: Update pre-release

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -6,5 +6,6 @@ set -eu -o pipefail # -x: is for debugging
 NEXT_VERSION=$(git rev-parse HEAD)
 sed -i "s/Version: '.*'/Version: '${NEXT_VERSION}'/" ./src/constants.ts
 npm run build
+git checkout ./src/constants.ts
 git add ./lib/index.js
 git commit -m "chore: npm run build"
