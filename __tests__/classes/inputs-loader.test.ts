a9b73bc78006cabcb036b2b041b52a3dced38785
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-07-25 17:15:59 +0900

    test: InputsLoader

--- __tests__/classes/inputs-loader.test.ts
+++ __tests__/classes/inputs-loader.test.ts
@@ -2,10 +2,12 @@ import fs from 'fs';
 
 import yaml from 'js-yaml';
 
+import {Inputs, InputsLoader} from '../../src/classes/inputs-loader';
+
 beforeEach(() => {
   jest.resetModules();
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const doc: any = yaml.load(fs.readFileSync(__dirname + '/../action.yml', 'utf8'));
+  const doc: any = yaml.load(fs.readFileSync(__dirname + '/../../action.yml', 'utf8'));
   Object.keys(doc.inputs).forEach(name => {
     const envVar = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`;
     process.env[envVar] = doc.inputs[name]['default'];
@@ -14,7 +16,7 @@ beforeEach(() => {
 
 afterEach(() => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const doc: any = yaml.load(fs.readFileSync(__dirname + '/../action.yml', 'utf8'));
+  const doc: any = yaml.load(fs.readFileSync(__dirname + '/../../action.yml', 'utf8'));
   Object.keys(doc.inputs).forEach(name => {
     const envVar = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`;
     delete process.env[envVar];
@@ -24,28 +26,26 @@ afterEach(() => {
 test('get default inputs', () => {
   process.env['INPUT_GITHUB_TOKEN'] = 'secret_token';
 
-  // const actionProcessor: ActionProcessor = new ActionProcessor();
+  const inputs: Inputs = new InputsLoader();
 
-  // const expextedInputs: Inputs = {
-  //   GithubToken: 'secret_token',
-  //   ConfigFilePath: '.github/label-commenter-config.yml'
-  // };
+  const expextedInputs: Inputs = {
+    GithubToken: 'secret_token',
+    ConfigFilePath: '.github/label-commenter-config.yml'
+  };
 
-  // expect(actionProcessor.inputs).toBe(expextedInputs);
-  expect('todo').toBe('todo');
+  expect(inputs).toEqual(expextedInputs);
 });
 
 test('get custom inputs', () => {
   process.env['INPUT_GITHUB_TOKEN'] = 'secret_token';
   process.env['INPUT_CONFIG_FILE'] = '.github/custom-label-commenter-config.yml';
 
-  // const actionProcessor: ActionProcessor = new ActionProcessor();
+  const inputs: Inputs = new InputsLoader();
 
-  // const expextedInputs: Inputs = {
-  //   GithubToken: 'secret_token',
-  //   ConfigFilePath: '.github/custom-label-commenter-config.yml'
-  // };
+  const expextedInputs: Inputs = {
+    GithubToken: 'secret_token',
+    ConfigFilePath: '.github/custom-label-commenter-config.yml'
+  };
 
-  // expect(actionProcessor.inputs).toBe(expextedInputs);
-  expect('todo').toBe('todo');
+  expect(inputs).toEqual(expextedInputs);
 });
