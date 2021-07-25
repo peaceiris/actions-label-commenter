7debb386058e34cd94184f763f5219e98bc0ec52
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-07-26 00:42:31 +0900

    Remove log prefix

--- src/logger.ts
+++ src/logger.ts
@@ -9,25 +9,25 @@ import {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 function groupConsoleLog(title: string, body: any): void {
-  startGroup(`[info] ${title}`);
+  startGroup(`${title}`);
   console.log(body);
   endGroup();
 }
 
 function info(message: string): void {
-  coreInfo(`[${coreInfo.name}] ${message}`);
+  coreInfo(`${message}`);
 }
 
 function debug(message: string): void {
-  coreDebug(`[${coreDebug.name}] ${message}`);
+  coreDebug(`${message}`);
 }
 
 function warning(message: string): void {
-  coreWarning(`[${coreWarning.name}] ${message}`);
+  coreWarning(`${message}`);
 }
 
 function error(message: string): void {
-  coreError(`[${coreError.name}] ${message}`);
+  coreError(`${message}`);
 }
 
 export {groupConsoleLog, info, debug, warning, error};
