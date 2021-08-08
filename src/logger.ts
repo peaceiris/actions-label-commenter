bb07fe69e1f31d49a6804dfbbd0ee20dee6758f3
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-08-09 04:07:14 +0900

    refactor: Remove unused functions

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,4 @@
-import {
-  startGroup,
-  endGroup,
-  info as coreInfo,
-  debug as coreDebug,
-  warning as coreWarning,
-  error as coreError
-} from '@actions/core';
+import {startGroup, endGroup, info as coreInfo} from '@actions/core';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 function groupConsoleLog(title: string, body: any): void {
@@ -18,16 +11,4 @@ function info(message: string): void {
   coreInfo(`${message}`);
 }
 
-function debug(message: string): void {
-  coreDebug(`${message}`);
-}
-
-function warning(message: string): void {
-  coreWarning(`${message}`);
-}
-
-function error(message: string): void {
-  coreError(`${message}`);
-}
-
-export {groupConsoleLog, info, debug, warning, error};
+export {groupConsoleLog, info};
