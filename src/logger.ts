07fd50ea8e1cf63a3dd021845806874cc55f0ec3
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-08-09 05:41:19 +0900

    feat: Enable groupConsoleLog in debug mode only

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,8 @@
-import {startGroup, endGroup, info as coreInfo} from '@actions/core';
+import {isDebug, startGroup, endGroup, info as coreInfo} from '@actions/core';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 function groupConsoleLog(title: string, body: any): void {
+  if (!isDebug()) return;
   startGroup(`${title}`);
   console.log(body);
   endGroup();
